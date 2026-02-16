257f558e0f83a70da3c61a9067c1a01e74e51666
binit <binitgupta.1711@gmail.com>
2026-02-16 20:09:59 +0530

    functions

---
+++ practice/functions/main.go
@@ -0,0 +1,27 @@
+package main
+
+	
+
+import "fmt"
+
+
+func add(a int, b int)int{
+	sum:=a+b
+	return sum
+}
+
+func addAdd(a, b, c int)int{
+	return a+b+c
+}
+
+func vals()(int, int){
+	return 3, 4 //Multiple returns
+}
+
+func main(){
+	fmt.Println(add(3, 4))
+	fmt.Println(addAdd(3, 4, 5))
+
+	a, b:= vals()
+	fmt.Println(a, b) 
+}
