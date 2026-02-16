d9d13660453cca66dc4e58e699f16fe87392b608
binit <binitgupta.1711@gmail.com>
2026-02-16 21:17:15 +0530

    variadic functions

--- practice/functions/main.go
+++ practice/functions/main.go
@@ -18,10 +18,24 @@ func vals()(int, int){
 	return 3, 4 //Multiple returns
 }
 
+func variadicSum(nums ...int)int{
+	total:=0
+	for _, num := range nums{
+		total += num
+	}
+	fmt.Println(total)
+
+	return total
+}
+
 func main(){
 	fmt.Println(add(3, 4))
 	fmt.Println(addAdd(3, 4, 5))
 
 	a, b:= vals()
-	fmt.Println(a, b) 
+	fmt.Println(a, b)
+	
+	nums := []int{1,2,3,4,5}
+
+	variadicSum(nums...)
 }
