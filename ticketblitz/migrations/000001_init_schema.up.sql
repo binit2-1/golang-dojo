3ba46c3c8a4d453d767c47216a5b9d13c9308f37
binit <binitgupta.1711@gmail.com>
2026-02-28 00:47:08 +0530

    feat: update Makefile for database URL variable and enhance migration scripts

--- ticketblitz/migrations/000001_init_schema.up.sql
+++ ticketblitz/migrations/000001_init_schema.up.sql
@@ -1,3 +1,26 @@
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS events;
-DROP TABLE IF EXISTS users;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- create users
+CREATE TABLE USERS(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user'
+);
+
+--create events
+CREATE TABLE EVENTS(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    total_tickets INT NOT NULL,
+    available_tickets INT NOT NULL
+);
+
+--create orders
+CREATE TABLE ORDERS(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    event_id UUID NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    status TEXT NOT NULL DEFAULT 'reserved',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
